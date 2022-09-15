8736de6fc814f1793b037968790597e016a9f5a5
Tanvir Taushif <tanvirpial9961@gmail.com>
2022-09-15 21:50:46 +0600

    Initial Commit

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Homepage.css';
+const Homepage = () => {
+    return (
+        <div>
+            <div className='top-page container-fluid'>
+                <h1>Bangladesh University of Professionals</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Homepage;
